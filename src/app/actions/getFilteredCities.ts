306286d7bcc13da4180f42b19e098427597abf62
Artem Sochinskiy <sochinskiyartom@gmail.com>
2024-11-05 13:46:56 +0300

    add env

--- src/app/actions/getFilteredCities.ts
+++ src/app/actions/getFilteredCities.ts
@@ -1,7 +1,7 @@
 import { City } from "../interfaces/City.interface";
 export async function getCities(): Promise<City[]> {
 	try {
-	    const response = await fetch("http://localhost:3000/api/cities");
+		const response = await fetch(process.env.API_URL+ "/api/cities");
 		const cities = await response.json() as City[];
 
 		// Фильтруем города с населением больше 50,000 и сортируем по алфавиту
@@ -16,6 +16,7 @@ export async function getCities(): Promise<City[]> {
 		filteredCities = filteredCities.filter(city => city.city !== cityWithMaxPopulation.city);
 
 		filteredCities = [cityWithMaxPopulation, ...filteredCities]
+		console.log(filteredCities)
 		return filteredCities;
 	} catch (error) {
 		console.error('Ошибка при загрузке городов:', error);
